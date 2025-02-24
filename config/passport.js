8e41ab55e2daf511acb8c8ecfa7f14bfbc67ef29
JUNAID PUTTEKKADAVAN <puttekkadavanjunaid@gmail.com>
2025-02-24 09:55:51 +0530

    google bug

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: 'https://junaid-puttekkadavan.site/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         // Search for the user with the Google ID
